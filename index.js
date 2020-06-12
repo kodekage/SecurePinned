422ca2807b7ec643a2a2b0bef9a7520993e983bf
Prosper Opara <oparaprosper79@gmail.com>
2020-06-12 08:36:05 +0100

    chore: Add view

--- index.js
+++ index.js
@@ -2,38 +2,44 @@
 
 require('dotenv').config();
 const { WebClient } = require('@slack/web-api');
-const ObjectsToCsv = require('objects-to-csv');
+const bodyParser = require('body-parser');
+const express = require('express');
 
-const options = {
-  channel: 'C012K8S66P5',
-  };
+const app = express();
+app.set('views', './views');
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true }))
+const web = new WebClient(process.env.SIMPLE_BOT_TOKEN);
 
-const web = new WebClient(process.env.BOT_TOKEN);
-(async  () => {
-  const pinned_messages = [];
+const options = { channel: 'CA100TWAW' };
+const pinned_messages = [];
+
+app.post('/pinned', async  (req, res) => {
+  console.log(req.form);
   try {
     const history = await web.conversations.history(options)
     history.messages.map((item, index) => {
-      if (item.pinned_info) {
-        pinned_messages.push([{
-          workspace_id: item.team,
-          data: {
-            id: item.client_msg_id,
-            channel: item.pinned_info.channel,
-            pinned_by: item.pinned_info.pinned_by,
-            time_stamp: item.pinned_info.pinned_ts,
-            message: item.text
-          }
-        }]);
-      }});
-    const csv = new ObjectsToCsv(pinned_messages);
-    const store_csv = await  csv.toDisk(`./document.csv`);
+      if (item.pinned_info) pinned_messages.push(item);
+    })
 
-    if (store_csv) console.log("CSV file created successfully!");
+    web.chat.postMessage({
+      channel: 'CA100TWAW',
+      text: `Looks like you lost the pinned messages again :confounded:\n\n Luckily I stored them for you :simple_smile: \n\n https://a4cb7242.ngrok.io/pinned/${options.channel}`
+    })
   } catch (e) {
     console.log(e);
   }
 
   console.log("Conversations retrieved successfully");
-})();
+  res.send('securePinned successfully processed your request');
+})
+
+app.get('/pinned/:channel', (req, res) => {
+  // console.log(req.params);
+  if (req.params.channel !== options.channel) return res.render('error', {message: "Channel does not exist, contact your workspace admin"})
+
+  res.render('index', {pinned_messages, channel: options.channel});
+})
+
+app.listen(process.env.PORT || 8000, () => console.log("Server is live on port 8000"));
 
